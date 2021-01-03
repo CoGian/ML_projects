6a405aa4d8c7272a6aa85f0cacaa455cd7c5a5ab
cogian <kgiantsios@atypon.com>
2021-01-03 22:28:34 +0200

    finish task 1

--- Project_final/task1.py
+++ Project_final/task1.py
@@ -1,9 +1,21 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_curve, auc
-from tqdm import tqdm
-from sklearn.cluster import KMeans, DBSCAN, SpectralClustering, AgglomerativeClustering
+from sklearn.cluster import KMeans, Birch
+from sklearn.mixture import GaussianMixture
 import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--algorithm",
+    "-a",
+    help="algorithm",
+)
+
+args = parser.parse_args()
+
+algorithm = args.algorithm
 
 pd.set_option('display.max_colwidth', 30)
 train_df = pd.read_csv("Task 1/NSL-KDDTrain.csv")
@@ -18,11 +30,12 @@ train_df = train_df.join(enc_df).drop(columns=one_hot_encoding_cols)
 enc_df = pd.DataFrame(enc.transform(test_df[one_hot_encoding_cols]).toarray())
 test_df = test_df.join(enc_df).drop(columns=one_hot_encoding_cols)
 
-X_train = train_df.values
+if algorithm == "kmeans" or algorithm == "birch" or algorithm == "gausian":
+    X_train = train_df.values
+
 x_cols = [column for column in test_df.columns if column != 'target']
 X_test = test_df[x_cols].values
 Y_test = test_df['target'].values
-Y_test = np.array([1 if target == "attack" else 0 for target in Y_test])
 
 print("x_train_shape: ", X_train.shape)
 print("x_test_shape: ", X_test.shape)
@@ -32,30 +45,37 @@ scaler = MinMaxScaler(feature_range=(0, 1))
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
+if algorithm == "kmeans":
+    clf = KMeans(n_clusters=2, random_state=42, verbose=0)
+elif algorithm == "birch":
+    clf = Birch(n_clusters=2, threshold=0.3)
+elif algorithm == "gausian":
+    clf = GaussianMixture(n_components=2, random_state=42, covariance_type="tied")
+else:
+    print("wrong algorithm")
+    exit(0)
 
-kmeans = KMeans(n_clusters=2, random_state=42, verbose=0)
-kmeans.fit(X_train)
+clf.fit(X_train)
 
-cluster_0_size = len([label for label in kmeans.labels_ if label == 0])
-cluster_1_size = len([label for label in kmeans.labels_ if label == 1])
+if algorithm != "gausian":
+    cluster_0_size = len([label for label in clf.labels_ if label == 0])
+    cluster_1_size = len([label for label in clf.labels_ if label == 1])
+else:
+    Y_train = clf.predict(X_train)
+    cluster_0_size = len([label for label in Y_train if label == 0])
+    cluster_1_size = len([label for label in Y_train if label == 1])
 
-print(cluster_0_size)
-print(cluster_1_size)
+print("cluster 0 size:", cluster_0_size)
+print("cluster 1 size:", cluster_1_size)
 
+positive_class = 1
+negative_class = 0
+if cluster_1_size > cluster_0_size:
+    positive_class = 0
+    negative_class = 1
 
-# sc = SpectralClustering(n_clusters=2, assign_labels="discretize", random_state=42)
-# sc.fit(X_train)
-# cluster_0_size = len([label for label in sc.labels_ if label == 0])
-# cluster_1_size = len([label for label in sc.labels_ if label == 1])
-#
-# print(cluster_0_size)
-# print(cluster_1_size)
+Y_test = np.array([positive_class if target == "attack" else negative_class for target in Y_test])
 
+Y_pred = clf.predict(X_test)
 
-ac = AgglomerativeClustering(n_clusters=2)
-ac.fit(X_train)
-cluster_0_size = len([label for label in ac.labels_ if label == 0])
-cluster_1_size = len([label for label in ac.labels_ if label == 1])
-
-print(cluster_0_size)
-print(cluster_1_size)
+print(accuracy_score(Y_test, Y_pred))
